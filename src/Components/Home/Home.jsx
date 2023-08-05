49fd26787981aefc95e11849a9e63cb9a771515b
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-08-05 17:40:13 +0530

    error fixed

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import "./home.css"
-import video from 'D:\Projects\dream-destiny\asset\vid1.mp4'
+// import video from 'D:\Projects\dream-destiny\asset\vid1.mp4'
 
 const Home = () => {
   return (
     <section className='home'>
       <div className='overlay'>home</div>
-      <video src={video}></video>
-      <h1>abhishek</h1>
+      {/* <video src={video}></video> */}
     </section>
   )
 }
