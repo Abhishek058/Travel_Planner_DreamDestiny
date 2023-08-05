724975438ee3b4d33d88bc6f083d532efbe287e5
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-08-05 19:59:53 +0530

    basic style of home created

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Navbar from './Components/Navbar/Navbar'
 import Home from "./Components/Home/Home"
 import Main from "./Components/Main/Main"
 import Footer from "./Components/Footer/Footer"
+import './app.css'
 
 function App() {
   return (
     <>
-    <Navbar/>
+    {/* <Navbar/> */}
     <Home/>
-    <Main/>
-    <Footer/>
+    {/* <Main/> */}
+    {/* <Footer/> */}
     </>
   )
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
-import "./home.css"
-// import video from 'D:\Projects\dream-destiny\asset\vid1.mp4'
+import React from "react";
+import "./home.css";
+import video from "../../asset/vid1.mp4";
+import { GrLocation } from "react-icons/gr";
 
 const Home = () => {
   return (
-    <section className='home'>
-      <div className='overlay'>home</div>
-      {/* <video src={video}></video> */}
-    </section>
-  )
-}
+    <section className="home">
+      <div className="overlay"></div>
+      <video src={video} muted autoPlay loop type="video/mp4"></video>
+      <div className="homeContent container">
+        <div className="textDiv">
+          <span className="smallText">Our package</span>
+          <h1 className="homeTitle">Search your Holidays</h1>
+        </div>
 
-export default Home
+        <div className="cardDiv grid">
+          <div className="destinationInput">
+            <label htmlFor="city">Search Your Destination:</label>
+            <div className="input flex">
+              <input type="text" placeholder="Enter name here..." />
+              <GrLocation className="icon"></GrLocation>
+            </div>
+          </div>
+          <div className="dateInput">
+            <label htmlFor="date">Select your date:</label>
+            <div className="input flex">
+              <input type="date" />
+            </div>
+          </div>
+          <div className="priceInput">
+            <div className="label_total flex">
+              <label htmlFor="price">Max price:</label>
+              <h3 className="total">$5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="5000" min="1000" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
