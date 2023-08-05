15e67cb98b43cf924fe9f173d4fb3ec5a21b1992
Lokesh1751 <101688217+Lokesh1751@users.noreply.github.com>
2023-08-05 16:13:08 +0530

    Basic Format Done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import "./app.css"
+import Navbar from './Components/Navbar/Navbar'
+import Home from "./Components/Home/Home"
+import Main from "./Components/Main/Main"
+import Footer from "./Components/Footer/Footer"
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <>
+    <Navbar/>
+    <Home/>
+    <Main/>
+    <Footer/>
+    </>
   )
 }
 
